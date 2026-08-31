d5505f971db50395527e10d16075f25886986c1f
DRUYIDka <reptiliaanna@gmail.com>
2026-08-31 23:51:50 +0300

    Initial commit

---
+++ src/main/kotlin/KS-3-1-String greeting.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var greeting = ""
+    val nameUser = "Anya"
+
+    greeting = "Good morning"
+    println("$greeting, $nameUser")
+    greeting = "Good evening"
+    println("$greeting, $nameUser")
+
+}
